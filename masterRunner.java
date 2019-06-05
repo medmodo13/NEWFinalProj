82af5b90314684404536c9ca536367e3f7445e11
JasminePParekh <jasmine.p.parekh@gmail.com>
2019-06-05 08:47:35 -0400

    Update masterRunner.java

--- masterRunner.java
+++ masterRunner.java
@@ -2,7 +2,7 @@ public class masterRunner{
 	public static void main(String args[])
     {
 
-		PerfectSquare ps = (double perfectSquaree)-> ((Math.sqrt(perfectSquaree) - Math.floor(Math.sqrt(perfectSquaree))) == 0);
+	PerfectSquare ps = (double perfectSquaree)-> ((Math.sqrt(perfectSquaree) - Math.floor(Math.sqrt(perfectSquaree))) == 0);
 
        System.out.println(ps.perfectSquare(64.0));
 
@@ -12,3 +12,13 @@ public class masterRunner{
 
 		}
 	}
+
+// What I learned (Jasmine)
+// The biggest thing that I learned about was the use of GitKraken and being able to view and manipulate the workflow.
+// Another important thing I learned was about how to use and branches and merge pull request to collaborate with other people on Comp Sci projects
+// Before my use of github was limited because I basically used it as an online code, storage locker 
+// Now I understand how powerful a tool like Github is for project collaboration in the Comp Sci field, especially
+// Lastly, I found the use of lambda notation to be very smart as well because it literally limited code volume to one line
+// I will definitely implement this much more in my code from now on
+// I feel that this project was very helpful as the final project because it taught me a real life skill that I will probably use in the near future 
+
