fd553db5c1cdc8afc1603494ff2fe386fcc32a9b
JasminePParekh <jasmine.p.parekh@gmail.com>
2019-06-05 07:41:36 -0400

    Update PerfectSquare.java

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -1,7 +1,3 @@
 public interface PerfectSquare{
-
-  public static boolean method(double f){
-     double sr = Math.sqrt(f);
-     return ((sr - Math.floor(sr)) == 0); 
-  } 
+    public boolean perfectSquare(int perfectSquare);
 }
